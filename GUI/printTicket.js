7fa13e065ebe6fe26c8849b89bdf4a8c023e0964
Zachary Zajdel <42820232+Zac-Zajdel@users.noreply.github.com>
2018-12-06 17:41:19 -0500

    PrintTicket is now working and introduced new feature in index

--- GUI/printTicket.js
+++ GUI/printTicket.js
@@ -1,20 +1,22 @@
-let array = new Array(20);
-for (let i = 0; i < array.length; i++) {
-  array[i] = false;
+let openSpot = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let closedSpot = [];
+
+const park = () => {
+  let remove = openSpot.shift();
+  closedSpot.push(remove);
+  closedSpot.sort();
+  return remove;
 }
 
-array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
-
-const parkingSpot = () => {
-  for (let i = 0; i < array.length; i++) {
-    if (i == false) {
-      return array[i];
-    }
-  }
+const leave = () => {
+  let remove = closedSpot.shift();
+  openSpot.push(remove);
+  openSpot.sort();
+  return remove;
 }
+// console.log(park());
+// console.log(park());
+// console.log(openSpot[0]);
+// console.log(closedSpot[0]);
+// console.log(leave());
 
-parkingSpot();
-
-const userLeave = (i) => {
-  return array[i] = true;
-}
