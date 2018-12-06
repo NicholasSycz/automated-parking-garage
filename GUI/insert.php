0952ba5043128d1a6d2ab6329666a18f7936edd6
Zachary Zajdel <42820232+Zac-Zajdel@users.noreply.github.com>
2018-12-06 14:46:15 -0500

    Added database functionality and final page

--- GUI/insert.php
+++ GUI/insert.php
@@ -1,11 +1,12 @@
 <?php
 include 'DataBaseConnection.php';
 
-// THIS IS ALL FUCKED BOYSSS
-// Store the day and the time the customer decided to choose our garage services. 
-$timestamp = date("Y-m-d H:i:s");
+// Stores the date and time into two variables. The time will be accessed from the database at a later time.
+// We will need the time to calculate the charges implemented on the customer.
+$time = date("H:i:s");
+$date = date("Y-m-d");
 
-$sql = "Insert into 001 (date) values ('$timestamp')";
+$sql = "Insert into records (time, date) values ('$time', '$date')";
 
 //Checks for a successful connection to the database.
 if($connection->query($sql) === TRUE) {
