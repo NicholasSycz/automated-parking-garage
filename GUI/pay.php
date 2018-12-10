4a41954b96e7f194d12aa95862a1d0254bacc5b6
Zachary Zajdel <42820232+Zac-Zajdel@users.noreply.github.com>
2018-12-10 15:14:20 -0500

    aa

--- GUI/pay.php
+++ GUI/pay.php
@@ -1,41 +1,47 @@
 <!doctype html>
 <!--
-  Departure screen to customer
+ Prompts the user to supply the ticket and then sens them to pay their bill
 -->
 <html class="bg-dark">
 
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="Tells customer goodbye">
+  <meta name="description" content="Prompts customer to give ticket and pay">
   <meta name="author" content="APGOD">
-  <meta http-equiv="refresh" content="5;url=http://localhost/automated-parking-garage/GUI/index.html" />
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
     crossorigin="anonymous">
-  <script type="text/javascript" src="printTicket.js"></script>
 
-  <title>Enjoy your day!</title>
+  <title>Welcome to Cornwell Parking</title>
 
 </head>
 
-<ticket class="text-center">
+<body class="text-center">
 
   <div class="d-flex h-100 p-3 mx-auto flex-column bg-dark text-white">
     <header class="masthead mb-auto">
       <div class="inner p-5">
         <div class="progress">
           <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75"
-            aria-valuemin="0" aria-valuemax="100" style="width: 100%">100%</div>
+            aria-valuemin="0" aria-valuemax="100" style="width: 50%">50%</div>
         </div>
         </nav>
       </div>
     </header>
 
     <main role="main" class="inner cover">
+      <h1 class="display-3 p-3">Insert your ticket below and select your parking number</h1>
       <p class="lead"></p>
-      <div id="Ticket">
-        <p class="display-2 p-3">Thank you for using our service</p>
-        <p class="display-2 p-3">Have a wonderful day!</p>
+      <div class="container">
+        <div class="row">
+          <div class="col align-self-center">
+            <form action="../Database/retrieveTime.php" method="POST">
+              <input class="form-control" type="number" value="000">
+              <p></p>
+              <input type="submit" class="btn btn-lg btn-primary m-4" value="Go to pay">
+            </form>
+          </div>
+        </div>
     </main>
 
     <footer class="fixed-bottom">
@@ -53,6 +59,6 @@
     crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
     crossorigin="anonymous"></script>
-</ticket>
+</body>
 
 </html>
